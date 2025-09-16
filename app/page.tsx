1f7b62309b4ce38c1d02cdd0d3493c1aba6e0952
blackcave0 <i3intel@protonmail.com>
2025-09-17 04:02:43 +0530

    refactor: simplify Page component by importing Home directly

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,5 @@
-// import Image from "next/image";
-// import Header from "./home/header";
-import HeroSection from "./home/HeroSection";
-// import StatsSection from "./home/StatsSection";
-import TrustedBySection from "./home/TrustedBySection";
-import SinglePlatform from "./home/SinglePlatform";
-import Footer from "./home/Footer";
-import SmartJobPosting from "./home/SmartJobPosting";
-import ApnaDatabase from "./home/ApnaDatabase";
-import ApnaCampus from "./home/ApnaCampus";
-import BalkWhatsapp from "./home/BulkWhatsapp";
-import ApnaEnterprise from "./home/ApnaEnterprise";
-import TalentPlaybook from "./home/TalentPlaybook";
-import WhyHirefrom from "./home/WhyHirefrom";
-import AskedQuestions from "./home/AskedQuestions";
-// import './style.css'
+import Home from './home/page';
 
-export default function Home() {
-  return (
-    <>
-      {/* <Header /> */}
-      <HeroSection />
-      {/* <StatsSection /> */}
-      <TrustedBySection />
-      <SinglePlatform />
-      <SmartJobPosting />
-      <ApnaDatabase />
-      <ApnaCampus />
-      <BalkWhatsapp />
-      <ApnaEnterprise />
-      <TalentPlaybook />
-      <WhyHirefrom />
-      <AskedQuestions />
-      <Footer />
-    </>
-  );
+export default function Page() {
+  return <Home />;
 }
